87ccc3119102ba6c6fc991196dc670d4aa2255c5
1gilmar <1gilmar@gmail.com>
2025-01-18 18:59:17 -0300

    ajustado uai

--- src/app/api/[...route]/route.ts
+++ src/app/api/[...route]/route.ts
@@ -1,23 +1,28 @@
-import { db as prisma } from '@/app/lib/db'
+import CataasService from '@/app/services/CataanService'
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+import SegredosRepository from '../repository/SegredosRepository'
+import SegredoController from '@/app/controllers/SegredoController'
 export const dynamic = 'force-dynamic'
 
 const app = new Hono().basePath('/api')
+
+const cataasService = new CataasService('https://cataas.com/cat?json=true')
+const segredosRepository = new SegredosRepository()
+const segredoController = new SegredoController(
+  cataasService,
+  segredosRepository
+)
 
 app.get('/hello', c => {
   return c.json({ msg: 'hi :)' })
 })
 
-app.post('/segredos', async ctx => {
-  const { nome } = await ctx.req.json()
-  return ctx.json({ usuario: nome })
-})
+app.get('/segredos', ctx => segredoController.getSegredos(ctx))
 
-app.get('/segredos', async ctx => {
-  const dados = await prisma.segredos.findMany({ take: 88 })
-  return ctx.json({ data: dados })
-})
+app.get('/segredos/:id', async ctx => segredoController.getSegredoById(ctx))
+
+app.post('/segredos', ctx => segredoController.createSegredo(ctx))
 
 // export default handle(app)
 export const GET = handle(app)
