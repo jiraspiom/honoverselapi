d0dea8bb16bfeb739a4243083d3dbef5c0d0a6ba
1gilmar <1gilmar@gmail.com>
2025-01-18 17:17:00 -0300

    ok bando de dados

--- src/app/api/[...route]/route.ts
+++ src/app/api/[...route]/route.ts
@@ -1,16 +1,31 @@
+import { PrismaClient } from '@prisma/client'
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 export const dynamic = 'force-dynamic'
 
 const app = new Hono().basePath('/api')
 
+const prisma = new PrismaClient()
+
 app.get('/hello', c => {
   return c.json({ msg: 'ok' })
 })
 
-app.get('/:id', c => {
-  const id = c.req.param('id')
-  return c.json({ msg: `id e ${id}` })
+app.get('/teste/:id', async ctx => {
+  const id = ctx.req.param('id')
+  return ctx.json({ msg: `id e ${id}` })
 })
 
+app.post('/teste', async ctx => {
+  const { nome } = await ctx.req.json()
+  return ctx.json({ usuario: nome })
+})
+
+app.get('/usuario', async ctx => {
+  const usuario = prisma.usuario.findMany()
+  return ctx.json({ data: usuario })
+})
+
+// export default handle(app)
 export const GET = handle(app)
+export const POST = handle(app)
